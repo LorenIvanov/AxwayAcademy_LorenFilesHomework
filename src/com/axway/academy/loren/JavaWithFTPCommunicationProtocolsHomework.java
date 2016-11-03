6c3da23ceb4ad270e87b594bec4125d0a60bfb79
LorenIvanov <loren.i.ivanov@gmail.com>
2016-11-03 14:23:36 +0200

    Second homework

---
+++ src/com/axway/academy/loren/JavaWithFTPCommunicationProtocolsHomework.java
@@ -0,0 +1,199 @@
+package com.axway.academy.loren;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+/**
+ * CRUD operations using FTP connection to server
+ *
+ * @author LorenIvanov
+ *
+ */
+public class JavaWithFTPCommunicationProtocolsHomework {
+
+	private Scanner userInput = new Scanner(System.in);
+	FTPClient client = null;
+
+	// login credentials
+	private String server = "localhost";
+	private String username = "LorenServer";
+	private String password = "123";
+
+	public JavaWithFTPCommunicationProtocolsHomework() {
+		try {
+			FTPConnection();
+			CRUDmenu();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (client != null && client.isConnected()) {
+				try {
+					client.disconnect();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * CRUD menu for user
+	 * 
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	protected void CRUDmenu() throws IOException, Exception {
+		boolean menuCycle = true;
+		System.out.println("This is CRUD menu. Please select an option.");
+		System.out.println("1. List files inside the directory.");
+		System.out.println("2. Download file.");
+		System.out.println("3. Upload file.");
+		System.out.println("4. Delete file.");
+		System.out.println("0. Exit.");
+		int userChoise = 0;
+		userChoise = userIntegerInputCheck(userChoise);
+		switch (userChoise) {
+		case 1:
+			listFiles();
+			break;
+		case 2:
+			downloadFile();
+			break;
+		case 3:
+			uploadFile();
+			break;
+		case 4:
+			deleteFile();
+			break;
+		case 0:
+			menuCycle = false;
+			break;
+		default:
+			CRUDmenu();
+			break;
+		}
+		if (menuCycle) {
+			CRUDmenu();
+		}
+	}
+
+	/**
+	 * Validates user input
+	 * 
+	 * @param userChoise
+	 * @return
+	 */
+	private int userIntegerInputCheck(int userChoise) {
+		while (!userInput.hasNextInt()) {
+			System.out.println("Please enter valid number.");
+			userInput.next();
+		}
+		userChoise = userInput.nextInt();
+		userInput.nextLine();
+		return userChoise;
+	}
+
+	/**
+	 * Deleting file from server
+	 * 
+	 * @throws Exception
+	 * @throws IOException
+	 */
+	private void deleteFile() throws IOException, Exception {
+		System.out.println("Choose file to delete:");
+		listFiles();
+		String fileToDelete = userInput.nextLine();
+		boolean success = client.deleteFile(fileToDelete);
+		System.out.println(success ? "Succeed to delete " + fileToDelete
+				: "Deleting failed");
+	}
+
+	/**
+	 * User uploads file to server
+	 * 
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	private void uploadFile() throws IOException, Exception {
+		System.out
+				.println("Enter the location of the file you want to upload.");
+		String fileName = userInput.nextLine();
+		InputStream in = new FileInputStream(fileName);
+		boolean success = client.storeFile(fileName, in);
+		// boolean success = client.appendFile(fileName, in);
+		System.out.println(success ? "Succeed to upload " + fileName
+				: "Uploading failed");
+		if (in != null) {
+			in.close();
+		}
+	}
+
+	/**
+	 * Downloads file
+	 * 
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	private void downloadFile() throws IOException, Exception {
+		System.out.println("Enter the name of the file you want to download.");
+		String fileName = userInput.nextLine();
+		OutputStream out = new FileOutputStream(new File(fileName));
+		boolean success = client.retrieveFile(fileName, out);
+		System.out.println(success ? "Succeed to download " + fileName
+				: "Downloading failed");
+		if (out != null) {
+			out.close();
+		}
+	}
+
+	/**
+	 * Lists all files on the server
+	 * 
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	private void listFiles() throws IOException, Exception {
+		FTPFile[] files = null;
+		files = client.listFiles();
+		String[] fileNames = client.listNames();
+		if (files != null && fileNames.length > 0) {
+			for (String aFile : fileNames) {
+				System.out.println(aFile);
+			}
+		}
+	}
+
+	/**
+	 * Creates FTP connection to the server (localhost)
+	 *
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	private void FTPConnection() throws IOException, Exception {
+		client = new FTPClient();
+
+		// connect to the server and authenticate
+		client.connect(server);
+		client.login(username, password);
+	}
+
+	/**
+	 * Creates JavaWithCommunicationProtocolsHomeworkTwo object
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new JavaWithFTPCommunicationProtocolsHomework();
+	}
+
+}
---
+++ src/com/axway/academy/loren/JavaWithHTTPCommunicationProtocolsHomework.java
@@ -0,0 +1,130 @@
+package com.axway.academy.loren;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * @author LorenIvanov
+ *
+ * 
+ */
+public class JavaWithHTTPCommunicationProtocolsHomework {
+
+	public JavaWithHTTPCommunicationProtocolsHomework() {
+		SSSLHandshake();
+		connectingToGoogle();
+	}
+
+	/**
+	 * Connecting to www.google.bg and getting response
+	 */
+	protected void connectingToGoogle() {
+		HttpURLConnection connection = null;
+		try {
+			// initialize the resource
+			URL googleUrl = new URL(
+					"https://www.google.bg/?gfe_rd=cr&ei=Oe0ZWPzDGqyz8wekibiwCw");
+			connection = (HttpURLConnection) googleUrl.openConnection();
+			// set request method
+			connection.setRequestMethod("GET");
+
+			// execute the request and get status code and message
+			int statusCode = connection.getResponseCode();
+			writeToFile(statusCode, connection.getResponseMessage());
+			System.out.println("The status code is " + statusCode);
+			System.out.println("Response message is "
+					+ connection.getResponseMessage());
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+	}
+
+	/**
+	 * Writes to file
+	 * 
+	 * @param statusCode
+	 * @param responseMessage
+	 */
+	private void writeToFile(int statusCode, String responseMessage) {
+		Path filePath = Paths.get("googleResponse.txt");
+		String response = "" + statusCode;
+		response = response.concat(" ").concat(responseMessage);
+		try {
+			Files.write(filePath, response.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void SSSLHandshake() {
+
+		// Some SSL Handshake magic stuff happening here
+		// This code segment makes connections over SSL possible
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(
+					java.security.cert.X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(
+					java.security.cert.X509Certificate[] certs, String authType) {
+			}
+		} };
+
+		HostnameVerifier allHostsValid = new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		};
+
+		// Install the all-trusting trust manager
+		try {
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection
+					.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+		} catch (Exception e) {
+		}
+		// SSL handshake magic ends here
+		// SSL handshake will be subject to future topics
+
+		// Set to perform authentication
+		Authenticator.setDefault(new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("test", "123".toCharArray());
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		new JavaWithHTTPCommunicationProtocolsHomework();
+	}
+
+}
